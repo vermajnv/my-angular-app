08ca3ee4e404361029898ff9aeacaab8402dce47
nayan <nayanrahul.jnv@gmail.com>
2024-05-09 17:00:59 +0530

    Completed first assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule
