af5b80038d9485b10238f17824d714ffe36bf7b6
nayan <nayanrahul.jnv@gmail.com>
2024-05-10 13:49:20 +0530

    Added blogs

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,14 +1,40 @@
 import { Component } from "@angular/core";
 
+type blogType = {title : string, description : string}[];
+
 @Component({
     selector : 'app-blogs',
     templateUrl : './blogs.component.html',
     styleUrls : ['./blogs.component.css']
 })
-export class BlogsComponent {
-    blogTitle : string = 'This is the blog title';
 
-    getBlogStatus() : string {
-        return 'Up and Running'
+export class BlogsComponent {
+    inputTitle : string;
+    addButtonStatus : boolean = false;
+    blogs : blogType = [ 
+        {
+            title : 'First blog',
+            description : 'This is descrption for first blog'
+        },
+        {
+            title : 'Second Blog',
+            description : 'Description for second blog is here'
+        }
+    ];
+
+    addTitle()
+    {
+        this.blogs.push({ title : this.inputTitle, description : 'Description'})
+        this.inputTitle = '';
+        this.changeButtonStatus()
+    }
+    
+    changeButtonStatus()
+    {
+        if(this.inputTitle.length > 0)
+        {
+            return this.addButtonStatus = true;
+        }
+        return this.addButtonStatus = false
     }
 }
